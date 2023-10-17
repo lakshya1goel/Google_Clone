e84b1e8617aa8e6a3a7981c4946f5f10fc76d945
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-10-17 10:56:56 +0530

    add basic body structure having search bar

--- lib/Mobile/mobile_layout_home_page.dart
+++ lib/Mobile/mobile_layout_home_page.dart
@@ -9,57 +9,111 @@ class MobileLayoutHomePage extends StatefulWidget {
 
 class _MobileLayoutHomePageState extends State<MobileLayoutHomePage> {
   @override
+
+  List<String> lang= ['हिन्दी', 'বাংলা', 'తెలుగు', 'मराठी', 'தமிழ்', 'ગુજરાતી', 'ಕನ್ನಡ', 'മലയാളം', 'ਪੰਜਾਬੀ'];
+
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          leading: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.menu,
-                  color: Colors.grey,
-                ),
-                onPressed: () {},
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.menu,
+                color: Colors.grey,
               ),
-            ],
-          ),
-          title: TabBar(
+              onPressed: () {},
+            ),
+          ],
+        ),
+        title: DefaultTabController(
+          length: 2,
+          child: TabBar(
             tabs: [
               Tab(text: 'ALL'), // First tab named 'All'
               Tab(text: 'IMAGES'), // Second tab named 'Images'
             ],
           ),
-          actions: [
-            Row(
-              children: [
-                Container(
-                    width: 20,
-                    height: 20,
-                    child: NineDotMatrixIcon()
-                ),
-                SizedBox(
+        ),
+        actions: [
+          Row(
+            children: [
+              Container(
                   width: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Container(
-                    height: 35,
-                    width: 35,
-                    child: Center(
-                      child: Text("L",
-                        style: TextStyle(color: Colors.white),
-                      ),
+                  height: 20,
+                  child: NineDotMatrixIcon()
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  height: 35,
+                  width: 35,
+                  child: Center(
+                    child: Text("L",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
+                  ),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("lib/assets/Images/google.png",
+              height: 50,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 300,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                border: Border.all(color: Colors.grey),
+                color: Colors.grey[800],
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  focusedBorder: InputBorder.none,
+                  prefixIcon: Icon(Icons.search,
+                    color: Colors.grey,
+                  ),
+              ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Text("Google offered in:",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            Wrap(
+              alignment: WrapAlignment.center,
+              children: List.generate(
+                lang.length, (index) => Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    lang[index],
+                    style: TextStyle(
+                      color: Colors.blue,
                     ),
                   ),
                 ),
-              ],
+              ),
             )
           ],
         ),
@@ -67,6 +121,8 @@ class _MobileLayoutHomePageState extends State<MobileLayoutHomePage> {
     );
   }
 }
+
+
 
 class NineDotMatrixIcon extends StatelessWidget {
   @override
